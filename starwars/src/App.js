615840491ed744461665688c6175cdf24766ce90
Moaney <ladonnamoney@gmail.com>
2019-08-30 21:04:22 -0400

    completed styling

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -16,20 +16,32 @@ const App = () => {
       .catch(err => console.log(err));
   }, []);
 
+  const Div = styled.div `
+  margin: auto;
+  box-sizing: border-box;
+  width: 400px;
+  height: 200px;
+  padding: 30px;  
+  border: 10px solid goldenrod;
+  color: navy;
+  `
+
   const H1 = styled.h1 `
   font-family: 'Russo One', sans-serif;
-
   `
 
   const H2 = styled.h2 `
   font-family: 'Saira', sans-serif;
+  font-size: 2rem;
   `
   const H3 = styled.h3 `
   font-family: 'Khand', sans-serif;
+  font-size: 1.4rem;
   `
 
   const P = styled.p `
   font-family: 'Bahianita', cursive;
+  font-size: 1.8rem;
   `
 
   return (
@@ -38,10 +50,10 @@ const App = () => {
         <H2>Star Wars Characters:</H2>
         <div>
           {data.map((person, index) => (
-            <div key={index}>
+            <Div key={index}>
               <H3>{person.name}</H3>
               <P>{person.birth_year}</P>
-              </div>
+              </Div>
           ))}
           </div>
         </div>
